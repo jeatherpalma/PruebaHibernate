2540e2da77c8bc8e1a22eac9e39ea6f12d2eab49
Jeather <loweroner33@gmail.com>
2017-09-25 15:21:58 -0500

    Se agregaron funciones que ejecutan procedimientos almacenados

--- src/main/java/GetAddAllData.java
+++ src/main/java/GetAddAllData.java
@@ -2,10 +2,13 @@
 import com.worknest.pruebahibernate.entity.Person;
 import com.worknest.pruebahibernate.util.HibernateUtil;
 import java.util.List;
+import javax.persistence.ParameterMode;
+import javax.persistence.StoredProcedureQuery;
 import javax.persistence.criteria.CriteriaQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 
 public class GetAddAllData {
@@ -24,6 +27,21 @@ public class GetAddAllData {
 		System.out.println("Name: " + persona.getFname());
                 System.out.println("Name: " + persona.getLname());
         }
+        
+        /*StoredProcedureQuery query = session.createNamedStoredProcedureQuery("insert_jeather");
+        query.setParameter("fname", "Yair");
+        query.setParameter("lname", "Herrera");
+        query.execute();*/
+        
+        
+        /*StoredProcedureQuery query = session.createNamedStoredProcedureQuery("todos_jeather");
+        query.setParameter("id", (long)2);
+        query.execute();
+        
+        String lname = (String)query.getOutputParameterValue("retorno");
+        System.out.println("LNAME: " + lname);*/
+	
+        
         
         
         transaction.commit();
--- target/classes/com/worknest/pruebahibernate/entity/Person.java
+++ target/classes/com/worknest/pruebahibernate/entity/Person.java
@@ -1,16 +1,40 @@
 
 package com.worknest.pruebahibernate.entity;
 
+import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedNativeQuery;
+import javax.persistence.NamedStoredProcedureQuery;
+import javax.persistence.ParameterMode;
+import javax.persistence.StoredProcedureParameter;
 import javax.persistence.Table;
 
+
+/*@NamedStoredProcedureQuery(
+	name = "insert_jeather", 
+	procedureName = "insert_jeather", 
+	parameters = { 
+		@StoredProcedureParameter(mode = ParameterMode.IN, type = String.class, name = "fname"), 
+		@StoredProcedureParameter(mode = ParameterMode.IN, type = String.class, name = "lname") 
+		//@StoredProcedureParameter(mode = ParameterMode.OUT, type = Double.class, name = "sum")
+	}
+)*/
+
+@NamedStoredProcedureQuery(
+	name = "todos_jeather", 
+	procedureName = "todos_jeather", 
+	parameters = { 
+		/*@StoredProcedureParameter(mode = ParameterMode.IN, type = Long.class, name = "id"), */
+                @StoredProcedureParameter(mode = ParameterMode.OUT, type = String.class, name = "retorno")
+	}
+)
 @Entity
 @Table(name = "person")
-public class Person {
+public class Person implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
